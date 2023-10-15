c31bfe129f2b8f44468b241f55952d3b7f9a035f
“mostafa-amr” <mamr1527@gmail.com>
2023-10-15 17:23:57 +0300

    not important

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productsController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,16 +20,16 @@ Route::get('/', function () {
 });
 
 
-Route::get("/userHome",[productsController::class,"index"])->name("index");
-Route::get("/myOrderUser",[productsController::class,"orders"])->name("userOrder");
+Route::get("/userHome", [productsController::class, "index"])->name("index");
+Route::get("/myOrderUser", [productsController::class, "orders"])->name("userOrder");
 
-Route::get("/adminHome",[productsController::class,"adminIndex"])->name("adminIndex");
-Route::get("/adminOrders",[productsController::class,"adminProducts"])->name("adminProducts");
-Route::get("/adminUser",[productsController::class,"adminUser"])->name("adminUser");
-Route::get("/adminManualOrder",[productsController::class,"adminManualOrder"])->name("adminManualOrder");
-Route::get("/adminChecks",[productsController::class,"adminChecks"])->name("adminChecks");
-Route::get("/adminAddUser",[productsController::class,"addUser"])->name("addUser");
-Route::get("/adminAddProduct",[productsController::class,"addProduct"])->name("AddProduct");
+Route::get("/adminHome", [productsController::class, "adminIndex"])->name("adminIndex");
+Route::get("/adminOrders", [productsController::class, "adminProducts"])->name("adminProducts");
+Route::get("/adminUser", [productsController::class, "adminUser"])->name("adminUser");
+Route::get("/adminManualOrder", [productsController::class, "adminManualOrder"])->name("adminManualOrder");
+Route::get("/adminChecks", [productsController::class, "adminChecks"])->name("adminChecks");
+Route::get("/adminAddUser", [productsController::class, "addUser"])->name("addUser");
+Route::get("/adminAddProduct", [productsController::class, "addProduct"])->name("AddProduct");
 
 // Route::get("userHome",[productsController::class,"index"])->name("index");
 // Route::get("myOrderUser",[productsController::class,"orders"])->name("userOrder");
@@ -40,4 +42,3 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
